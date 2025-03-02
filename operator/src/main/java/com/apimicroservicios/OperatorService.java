5eaec834b841420dabd21a7d05d6e579f60bcf42
Javier Gonzalez Pena <gonzalezp1004@gmail.com>
2025-03-02 00:43:40 -0600

    Docker container working with dockerfiles for each microservice and ompose.yaml for all services

--- operator/src/main/java/com/apimicroservicios/OperatorService.java
+++ operator/src/main/java/com/apimicroservicios/OperatorService.java
@@ -2,7 +2,6 @@ package com.apimicroservicios;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 import com.apimicroservicios.Datos.Datos;
 
@@ -11,8 +10,6 @@ public class OperatorService {
 
     @Autowired
     private OperatorRepository operadorRepository;
-
-    private final String buscadorServiceUrl = "http://localhost:8080/search-movies"; // Nombre del microservicio "Buscador" en Eureka
 
     public Object obtenerDatos(Long id) {
         // Lógica para obtener datos desde la base de datos
@@ -25,9 +22,6 @@ public class OperatorService {
 
         // Lógica para insertar datos en la base de datos
         operadorRepository.save(datos);
-        // Después de insertar, notificamos al microservicio de búsqueda para indexar los datos
-        RestTemplate restTemplate = new RestTemplate();
-        restTemplate.postForObject(buscadorServiceUrl + "/peliculas", datos, Object.class);
         return datos;
     }
 
